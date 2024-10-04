5853a069d889df4538ea217905a86cc4d430158e
VAmanJain <vaman.jain17@gmail.com>
2024-10-04 10:31:04 +0530

    resolving router issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,56 @@
 
+// // App.tsx
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+// import { Outlet } from 'react-router-dom';
+
+// const App=()=> {
+//   return (
+//     <>
+//       <div className="m-0 p-0 pb-6 primary_color">
+//         <Navbar />
+//         <Outlet/>
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
 // App.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { Outlet } from 'react-router-dom';
+import Home from "@/components/Home";
+import { DashboardComponent } from "@/components/dashboardCandidate";
+import { DashboardComponentEmployer } from "@/components/dashboard"; // Fixed typo: DashboardComponentEmploye
+import { LeaderboardPage } from "@/components/leaderboard";
+import { ProfilePage } from "@/components/profile";
+import { VerificationPage } from "@/components/verification";
+import { SettingsPage } from "@/components/settings";
+import { PostJobPage } from "@/components/post-job";
+import RegisterationPage from "@/components/RegisterationPage";
+import CertificateSubmissionComponent from "@/components/CertificationSubmission";
 
-const App=()=> {
+const App = () => {
   return (
     <>
-      <div className="m-0 p-0 pb-6 primary_color">
-        <Navbar />
-        <Outlet/>
-        <Footer />
-      </div>
+      <Navbar />
+      <Routes> {/* Wrap all Route components in Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard/candidate" element={<DashboardComponent />} />
+        <Route path="/dashboard/employer" element={<DashboardComponentEmployer />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/verifications" element={<VerificationPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/post-jobs" element={<PostJobPage />} />
+        <Route path="/certificate-submission" element={<CertificateSubmissionComponent />} />
+        <Route path="/register" element={<RegisterationPage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,18 +4,24 @@ import "./index.css";
 import { MetaMaskProvider } from "metamask-react";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
-import { RouterProvider } from "react-router-dom"; // Import RouterProvider
-import Router from './Router/Router'; // Import your router
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-  
+import { BrowserRouter, RouterProvider } from "react-router-dom"; // Import RouterProvider
+import Router from "./Router/Router"; // Import your router
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App.tsx";
+
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <StrictMode>
     <Provider store={store}>
       <MetaMaskProvider>
-        <RouterProvider router={Router} /> {/* Use RouterProvider here */}
+        {/* <RouterProvider router={Router} /> Use RouterProvider here */}
+        <BrowserRouter>
+          {" "}
+          {/* Wrap your App in BrowserRouter */}
+          <App /> {/* Render the main App component */}
+        </BrowserRouter>
       </MetaMaskProvider>
     </Provider>
   </StrictMode>
